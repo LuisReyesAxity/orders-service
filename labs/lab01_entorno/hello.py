cb83488a27f49281d2dee27255c8ed679291d3e5
LuisReyesAxity <112987252+LuisReyesAxity@users.noreply.github.com>
2026-02-20 16:24:04 -0600

    Add labs folder structureLab01: entorno configurado - black, isort, ruff, pre-commit

---
+++ labs/lab01_entorno/hello.py
@@ -0,0 +1,12 @@
+#Hola mundo
+print("Hola Python")
+
+#aqui conocemos el tipo de dato con la palabra type, e imprime esto <class 'str'>
+print(type("Hola mundo")) # TIPO 'srt'
+print(type(5)) # TIPO 'int'
+
+"""
+comentarios varias lineas
+comentarios varias lineas
+comentarios varias lineas
+"""
---
+++ labs/lab01_entorno/main.py
@@ -0,0 +1,14 @@
+x = 1
+y = 2
+
+
+def suma(a, b):
+    return a + b
+
+
+class miClase:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self):
+        print("Hola " + self.nombre)
