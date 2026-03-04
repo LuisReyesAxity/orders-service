ed71553bdbf5ef12c5acffa9792d789b263dfd20
LuisReyesAxity <112987252+LuisReyesAxity@users.noreply.github.com>
2026-03-04 01:52:35 -0600

    remove unused import OrderItem

--- src/orders_service/infrastructure/repository.py
+++ src/orders_service/infrastructure/repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Column, String
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from orders_service.domain.entities import Order, OrderItem, OrderStatus
+from orders_service.domain.entities import Order, OrderStatus
 
 
 # Base para modelos SQLAlchemy
